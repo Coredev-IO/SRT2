3a97a90bd9eb52119dbc6cadcc01038f4ea2cfeb
Octavio Martinez Campuzano <octavio.martinez@coredev.io>
2015-03-11 12:50:53 -0600

    test

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -22,9 +22,9 @@ class Welcome extends CI_Controller {
 	{
 		$this->load->helper('url');
 		$this->load->database();
-		$this->load->model('blog'); //cargamos modelo
+		$this->load->model('Blog'); //cargamos modelo
 
-		
+
 		$this->load->view('welcome_message');
 	}
 }
