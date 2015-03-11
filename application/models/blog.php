3e9d4e7f7751ceea086c2f7e59fa5c3154ded37a
Octavio Martinez Campuzano <octavio.martinez@coredev.io>
2015-03-11 12:45:50 -0600

    test

--- application/models/blog.php
+++ application/models/blog.php
@@ -2,26 +2,26 @@
 
 class Blog extends CI_Model {
 
-  public $variable;
+	public $variable;
 
-  public function __construct()
-  {
-    parent::__construct();
+	public function __construct()
+	{
+		parent::__construct();
+		
+	}
 
-  }
+	public function posts(){
+		$this->db->select('post_title, post_date, post_content, guid, post_type');
+		$this->db->from('wp_posts');
+		$this->db->where('post_status', 'publish');
+		$this->db->where('post_type', 'post');
+		$this->db->order_by("post_date", "desc"); 
+		$this->db->limit(6);
 
-  public function posts(){
-    $this->db->select('post_title, post_date, post_content, guid, post_type');
-    $this->db->from('wp_posts');
-    $this->db->where('post_status', 'publish');
-    $this->db->where('post_type', 'post');
-    $this->db->order_by("post_date", "desc");
-    $this->db->limit(6);
-
-    $query = $this->db->get();
+		$query = $this->db->get();
 
 
-    return $query->result();
-  }
+		return $query->result();
+	}
 
 }
