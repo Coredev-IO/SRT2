73104c05275aa17b873d405a0bad09b9cc8addda
Octavio Martinez Campuzano <octavio.martinez@coredev.io>
2015-03-11 13:07:44 -0600

    test

--- application/models/blog.php
+++ application/models/blog.php
@@ -9,18 +9,17 @@ class Blog extends CI_Model {
 	}
 
 	public function posts(){
-		// $this->db->select('post_title, post_date, post_content, guid, post_type');
-		// $this->db->from('wp_posts');
-		// $this->db->where('post_status', 'publish');
-		// $this->db->where('post_type', 'post');
-		// $this->db->order_by("post_date", "desc");
-		// $this->db->limit(6);
-		//
-		// $query = $this->db->get();
-		//
-		//
-		// return $query->result();
-		return "hola";
+		$this->db->select('post_title, post_date, post_content, guid, post_type');
+		$this->db->from('wp_posts');
+		$this->db->where('post_status', 'publish');
+		$this->db->where('post_type', 'post');
+		$this->db->order_by("post_date", "desc");
+		$this->db->limit(6);
+
+		$query = $this->db->get();
+
+
+		return $query->result();
 	}
 
 }
